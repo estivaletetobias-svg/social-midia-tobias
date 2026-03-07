c5fe7565bd0ecddacb949d2bab9c2d97d54bde5c
Tobias Estivalete <estivaletetobias@gmail.com>
2026-03-07 17:29:00 -0300

    chore: Remove debug files with mock youtube responses that triggered github API secret scanners

--- testyt_api.js
+++
@@ -1,9 +0,0 @@
-const TranscriptClient = require('youtube-transcript-api');
-
-async function test() {
-    try {
-        const list = await TranscriptClient.fetchTranscript('dQw4w9WgXcQ'); // not sure what method
-        console.log(list);
-    } catch (e) { console.error(e) }
-}
-test();
